6030fe7ff28a95e6a766fe328fb6f3a2492924eb
EthanRosenthal <ethanrosenthal@gmail.com>
2022-07-11 21:47:55 -0400

    Convert repo list to markdown table

--- starz.py
+++ starz.py
@@ -1,41 +1,59 @@
 import os 
 
 from github import Github
+import pandas as pd
 
-def leftpad(s, pad, total_width):
-    size = len(s)
-    to_pad = total_width - size 
-    return pad * to_pad + s 
+# A unicode star emoji
+STAR = u"\u2606"
 
 
 token = os.environ["PERSONAL_ACCESS_TOKEN"]
 gh = Github(token)
+
+# Find all repos that I made that are not a fork.
 repos = [
     repo for repo in gh.get_user().get_repos(visibility="public", affiliation="owner")
     if not repo.fork
 ]
-repos = sorted(repos, key=lambda x: -x.stargazers_count)
-repo_infos = []
-for repo in repos:
-    repo_infos.append(
-        f"{repo.stargazers_count} ⭐ [{repo.name}]({repo.html_url}): "
-        f"{repo.description if repo.description else ''} ({repo.created_at.year})"
-    )
+
+# Convert to a DataFrame
+repo_df = pd.DataFrame(
+    [
+        {
+            # The name is a markdown link.
+            "name":  f"[{repo.name}]({repo.html_url})", 
+            "description": repo.description if repo.description else "", 
+            STAR: repo.stargazers_count,
+            "created": repo.created_at.year
+        }
+        for repo in repos
+
+    ]
+)
+# Sort rows and order the columns.
+repo_df = repo_df.sort_values(STAR, ascending=False)
+repo_df = repo_df[[STAR, "name", "description", "created"]]
+
+repo_markdown = repo_df.to_markdown(index=False)
+
 
 with open("README.md", "r") as f:
     readme = f.read()
 
+# Replace the table in the README by finding this hacky start and end template.
 start_pattern = "<!-- BEGIN LIST -->"
 end_pattern = "<!-- END LIST -->"
 start_index = readme.find(start_pattern) + len(start_pattern)
 end_index = readme.find("<!-- END LIST -->")
+
 readme = (
-    readme[:start_index] + "\n- " + "\n- ".join(repo_infos) + "\n" + readme[end_index:]
+    readme[:start_index] + "\n" + repo_markdown + "\n" + readme[end_index:]
 )
 
-this_repo = repo = [r for r in repos if r.name == "EthanRosenthal"][0]
-contents = repo.get_contents("README.md", ref="main")
-repo.update_file(
+# Commit the results.
+this_repo = [r for r in repos if r.name == "EthanRosenthal"][0]
+contents = this_repo.get_contents("README.md", ref="main")
+this_repo.update_file(
     contents.path, 
     "Automatic Update From GitHub Actions", 
     readme, 
